2095ada8ec97ed88d0aca8f8a220c59bee383665
Jose L. Louis <louisjoselael@gmail.com>
2020-03-06 15:16:04 -0500

    updating

--- LargestNumberAtLeastTwiceOfOthers.cpp
+++ LargestNumberAtLeastTwiceOfOthers.cpp
@@ -6,27 +6,32 @@ using namespace std;
 
 class Solution {
 public:
+
+    static int getHighestIndex(vector<int> &nums, bool skip = false, const int skip_index = -1) {
+        int highest = 0;
+
+        for (auto i = 0; i < nums.size(); i++)
+            if (skip && i == skip_index)
+                continue;
+            else if (nums[i] > nums[highest])
+                highest = i;
+
+        return highest;
+    }
+
     static int dominantIndex(vector<int> &nums) {
         if (nums.size() < 2)
             return 0;
 
         int first = 0, second = 0;
-
-        for (auto i = 0; i < nums.size(); i++)
-            if (nums[i] > nums[first])
-                first = i;
-
-        for (auto i = 0; i < nums.size(); i++)
-            if (i == first)
-                continue;
-            else if (nums[i] > nums[second])
-                second = i;
+        first = getHighestIndex(nums);
+        second = getHighestIndex(nums, true, first);
 
         return (((nums[first]) >= (2 * nums[second])) || (nums[second] == 0)) ? first : -1;
     }
 };
 
 int main() {
-    vector<int> list{2, 0, 0, 3};
+    vector<int> list{1, 0};
     cout << Solution::dominantIndex(list) << endl;
 }
